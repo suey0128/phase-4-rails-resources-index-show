dfbbf7b8eb2812bc34a0b2c09b6e3b1cada2615f
suey0128 <syui0128@gmail.com>
2021-07-18 21:28:01 -0700

    commit tree edb40cda4f79622ef400d83d16632fc036e9805e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/birds' => 'birds#index'
+  # get '/birds/:id' => 'birds#show'
+  resources :birds, only: [:index, :show]
 end
